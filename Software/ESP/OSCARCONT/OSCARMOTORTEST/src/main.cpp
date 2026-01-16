7aa2d123014ce1c74bac3888c5921bc1d9e7858c
G_us <74196970+G-us@users.noreply.github.com>
2026-01-16 14:37:03 +0100

    Control Motors stRT

--- Software/ESP/OSCARCONT/OSCARMOTORTEST/src/main.cpp
+++ Software/ESP/OSCARCONT/OSCARMOTORTEST/src/main.cpp
@@ -1,29 +1,180 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com
+*********/
+
+// Load Wi-Fi library
+#include <WiFi.h>
 #include <Arduino.h>
-#include <Stepper.h>
+// Replace with your network credentials
+const char *ssid = "Henry's iPhone";
+const char *password = "karlstad12";
 
-// Number of steps per output rotation
-const int stepsPerRevolution = 200;
+// Set web server port number to 80
+WiFiServer server(80);
 
-// Create Instance of Stepper library
-Stepper myStepper(stepsPerRevolution, 12, 11, 10, 9);
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String motorAState = "off";
+String motorBState = "off";
+
+// Assign output variables to GPIO pins
+const int motorA = 14;
+const int motorB = 15;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup()
 {
-  // set the speed at 20 rpm:
-  myStepper.setSpeed(20);
-  // initialize the serial port:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(motorA, OUTPUT);
+  pinMode(motorB, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(motorA, LOW);
+  digitalWrite(motorB, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void loop()
 {
-  // step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
+  WiFiClient client = server.available(); // Listen for incoming clients
 
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(500);
+  if (client)
+  { // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client."); // print a message out in the serial port
+    String currentLine = "";       // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime)
+    { // loop while the client's connected
+      currentTime = millis();
+      if (client.available())
+      {                         // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out the serial monitor
+        header += c;
+        if (c == '\n')
+        { // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)
+          {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0)
+            {
+              Serial.println("GPIO 26 on");
+              motorAState = "on";
+              digitalWrite(motorA, HIGH);
+            }
+            else if (header.indexOf("GET /26/off") >= 0)
+            {
+              Serial.println("GPIO 26 off");
+              motorAState = "off";
+              digitalWrite(motorA, LOW);
+            }
+            else if (header.indexOf("GET /27/on") >= 0)
+            {
+              Serial.println("GPIO 27 on");
+              motorBState = "on";
+              digitalWrite(motorB, HIGH);
+            }
+            else if (header.indexOf("GET /27/off") >= 0)
+            {
+              Serial.println("GPIO 27 off");
+              motorBState = "off";
+              digitalWrite(motorB, LOW);
+            }
+
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+
+            // Display current state, and ON/OFF buttons for GPIO 26
+            client.println("<p>GPIO 26 - State " + motorAState + "</p>");
+            // If the motorAState is off, it displays the ON button
+            if (motorAState == "off")
+            {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            }
+            else
+            {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+            // Display current state, and ON/OFF buttons for GPIO 27
+            client.println("<p>GPIO 27 - State " + motorBState + "</p>");
+            // If the motorBState is off, it displays the ON button
+            if (motorBState == "off")
+            {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            }
+            else
+            {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          }
+          else
+          { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {                   // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
